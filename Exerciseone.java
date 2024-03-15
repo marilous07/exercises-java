07a688403fb5af00f741343f0004f58986dca3f8
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-15 18:16:11 +0200

    exercisetwo

--- Exerciseone.java
+++ Exerciseone.java
@@ -1,3 +1,11 @@
+/*Given an integer n, perform the following conditional actions:
+If is odd: print Weird
+If is even and in the inclusive range of 2 to 5: print Not Weird
+If is even and in the inclusive range of 6 to 20: print Weird
+If is even and greater than 20: print Not Weird
+ */
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -22,7 +30,7 @@ public class Exerciseone {
             } else if (N > 20) {
                 System.out.println("Not Weird");
             }
-        } 
+        }
 
         bufferedReader.close();
     }
