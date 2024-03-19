a15872aefb8d9d282a8ec9b44e0c04a739d35b3b
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-19 13:40:11 +0200

    2

--- Exercisefour.java
+++ Exercisefour.java
@@ -1,3 +1,17 @@
+/*
+Given an integer,N, print its first 10 multiples. Each multiple N x i(where 1<=i<=10) should be printed on a new line in the form: N x i = result.
+
+Input Format
+A single integer,N.
+
+Constraints
+2<=N<=20
+
+Output Format
+Print 10 lines of output; each line i (where 1<=i<=10) contains the result of N x i in the form:
+N x i = result.
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
