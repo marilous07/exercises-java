c89b24e5e9ce6e552ee0e5dcc0b017bc5b9911f7
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-19 14:03:40 +0200

    3

--- Exercisefour.java
+++ Exercisefour.java
@@ -23,7 +23,11 @@ public class Exercisefour {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
-
+        int result;
+        for (int i=1; i<=10; i++){
+            result = N*i;
+            System.out.println(N + "x" + i + "="+result);
+        }
         bufferedReader.close();
     }
 }
