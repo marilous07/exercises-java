369b50589917a970860d6c175b09e69f3188314a
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-20 20:55:39 +0200

    data type size check

--- Exercisesix.java
+++ Exercisesix.java
@@ -5,9 +5,29 @@ public class Exercisesix {
 
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
+        String a = "byte, short, int and long.";
+        String b = "short, int and long.";
+        String c = "int and long.";
+        String d = "long.";
             try {
                 int T = Integer.parseInt(in.nextLine());
-                System.out.println(T);
+                
+                if (T<100){
+                    System.out.println("The "+T+" can be fitted in "+a);
+                }
+                else if (T>=100){
+                    System.out.println("The "+T+" can be fitted in "+a);
+                }
+                else if(T>=10000){
+                    System.out.println("The "+T+" can be fitted in "+b);
+                }
+                else if(T>=100000){
+                    System.out.println("The "+T+" can be fitted in "+c);
+                }
+                else if(T>=1000000){
+                    System.out.println("The "+T+" can be fitted in "+d);
+                }
+
             } catch (Exception e) {
                     System.out.println("User input was not a number.");
             }
