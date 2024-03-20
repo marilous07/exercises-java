6d441d731ae6405a5f0ae347e24c0a09412ddfc0
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-20 20:15:30 +0200

    new-Exercise

---
+++ Exercisesix.java
@@ -0,0 +1,10 @@
+//Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+import java.util.Scanner;
+
+public class Exercisesix {
+
+    public static void main(String[] args) {
+        Scanner T = new Scanner (System.in);
+        
+    }
+}
