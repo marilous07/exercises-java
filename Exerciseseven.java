3864b7273f6c4ce4303ce14660adeb2b4474ab9c
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-21 14:55:48 +0200

    eof check

--- Exerciseseven.java
+++ Exerciseseven.java
@@ -1,8 +1,18 @@
-/*rean n lines of input until reach EOF, then number and print all n lines of content.*/
+/*read n lines of input until reach EOF, then number and print all n lines of content.*/
 
+
+import java.util.Scanner;
 
 public class Exerciseseven {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int lineNumber = 1;
 
+        while (in.hasNext()){
+            String line = in.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+        in.close();
     }
 }
