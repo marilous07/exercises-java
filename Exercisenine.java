4a372166cfce7fb8bc224cb5518dae191b0a957f
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-24 12:36:39 +0200

    1

--- Exercisenine.java
+++ Exercisenine.java
@@ -1,3 +1,19 @@
+//This Java program reads an integer n, converts it to a string s, and then checks if the string s matches the string representation of the integer n. If they match, it prints "Good job"; otherwise, it prints "Wrong answer"
+
+import java.util.Scanner;
+
 public class Exercisenine {
-    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        
+        if (s.equals(Integer.toString(n))) {
+            
+        } else {
+            
+        }
+
+        }
 }
