3e5959433355e09d50f1d44da9bccb38e35fba0c
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-24 12:37:21 +0200

    int -string s

--- Exercisenine.java
+++ Exercisenine.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Exercisenine {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String s = in.nextLine();
 
         
         if (s.equals(Integer.toString(n))) {
