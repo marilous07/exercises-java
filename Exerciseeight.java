19387859074948c3a740910e6bc5f7080841c230
Marilena Mousteri <marilous07.mm@gmail.com>
2024-03-24 12:12:37 +0200

    Static initialization blocks

--- Exerciseeight.java
+++ Exerciseeight.java
@@ -1,3 +1,26 @@
+//
+
+import java.util.Scanner;
+
+
 public class Exerciseeight {
-    
+    static int breadth;
+    static int height;
+
+    // Static initialization block
+    static {
+        Scanner in = new Scanner(System.in);
+        breadth = in.nextInt();
+        height = in.nextInt();
+        in.close();
+    }
+
+    public static void main(String[] args) {
+        if (breadth > 0 && height > 0) {
+            System.out.println(breadth * height);
+        } else {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        
+    }
 }
